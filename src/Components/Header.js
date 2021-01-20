fc5c6ee2ba7f3c1dd1044a327b921494bcb0e20a
Abhi Balijepalli <stickz@abhis-air.lan>
2021-01-19 19:33:26 -0800

    Cobweb - Blue

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg color = "#CF995B" num= {150} type="cobweb" bg={true} />
+      <ParticlesBg color = "#3B5FB3" num= {150} type="cobweb" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -27,7 +27,7 @@ class Header extends Component {
       </nav>
 
       <div className="row banner">
-      
+
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
             <h3>{description}.</h3>
