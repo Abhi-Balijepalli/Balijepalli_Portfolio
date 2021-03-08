1d083113f5254a685342ea3f3fc349e26b0166c6
Abhi Balijepalli <57305830+Abhi-Balijepalli@users.noreply.github.com>
2021-03-08 14:14:58 -0800

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-      <ParticlesBg color = "#CF995B" num= {50} type="square" bg={true} />
+      <ParticlesBg color = "#CF995B" num= {30} type="square" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
